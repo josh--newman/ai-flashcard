207b8321f9f1af96c07a46c79fe7805376c9e4bd
Josh Newman <josh.newman@zapier.com>
2023-04-13 14:53:36 +1000

    Add user avatar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
 const Header: React.FC = () => {
   const router = useRouter();
@@ -15,6 +16,12 @@ const Header: React.FC = () => {
   let right = session ? (
     <div>
       <p>
+        <Image
+          src={session.user.image}
+          alt="User Image"
+          width={50}
+          height={50}
+        />
         {session.user.name} ({session.user.email})
       </p>
     </div>
