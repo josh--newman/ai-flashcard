798c6677fa5311d0d6cb0cdc7cd46c6b287e185a
Josh Newman <josh.newman@zapier.com>
2023-04-13 13:31:06 +1000

    Create assignment on card creation

--- pages/api/card/index.ts
+++ pages/api/card/index.ts
@@ -8,13 +8,18 @@ export default async function handle(req, res) {
   const { front, back } = req.body;
 
   const session = await getServerSession(req, res, authOptions);
-  console.log({ session });
+
+  // Create a new card and assignment
   const result = await prisma.card.create({
     data: {
       front,
       back,
       User: { connect: { email: session?.user?.email } },
+      Assignment: {
+        create: {},
+      },
     },
   });
+
   res.json(result);
 }
