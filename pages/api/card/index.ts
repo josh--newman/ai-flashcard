a0394e77e7bafa3543fbe0fc643b579b99145b6a
Josh Newman <josh.newman4390@gmail.com>
2023-11-16 06:52:12 +1100

    Add error handling to card endpoint

--- pages/api/card/index.ts
+++ pages/api/card/index.ts
@@ -7,6 +7,10 @@ import prisma from "../../../lib/prisma";
 export default async function handle(req, res) {
   const { front, back } = req.body;
 
+  if (!front || !back) {
+    return res.status(400).json({ error: "Missing fields" });
+  }
+
   const session = await getServerSession(req, res, authOptions);
 
   // Create a new card and assignment
