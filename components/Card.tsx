042bcff103c534a0f5e2725743c1097244c5ca26
Josh Newman <josh.newman4390@gmail.com>
2023-11-20 21:35:55 +1100

    Flip animation on cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,4 +1,6 @@
-import { FC, useState } from "react";
+import styles from "./Card.module.css";
+import { useState } from "react";
+import { useSpring, animated } from "react-spring";
 import { Card as CardType } from "../types";
 
 type Props = {
@@ -6,20 +8,39 @@ type Props = {
   onGrade: (success: boolean) => void;
 };
 
-type CardSide = "front" | "back";
+const Card = ({ card, onGrade }: Props) => {
+  const [flipped, setFlipped] = useState(false);
+  const { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
 
-const Card: FC<Props> = ({ card, onGrade }) => {
-  const [currentSide, setCurrentSide] = useState<CardSide>("front");
-
-  return currentSide === "front" ? (
-    <div onClick={() => setCurrentSide("back")}>
-      <div>{card.front}</div>
-    </div>
-  ) : (
-    <div>
-      <div>{card.back}</div>
-      <button onClick={() => onGrade(false)}>❌</button>
-      <button onClick={() => onGrade(true)}>✅</button>
+  return (
+    <div
+      className={styles.container}
+      onClick={() => setFlipped((state) => !state)}
+    >
+      <animated.div
+        className={styles.card}
+        style={{ opacity: opacity.to((o) => 1 - o), transform }}
+      >
+        {card.front}
+      </animated.div>
+      <animated.div
+        className={styles.card}
+        style={{
+          opacity,
+          transform,
+          rotateY: "180deg",
+        }}
+      >
+        <>
+          <div>{card.back}</div>
+          <button onClick={() => onGrade(false)}>❌</button>
+          <button onClick={() => onGrade(true)}>✅</button>
+        </>
+      </animated.div>
     </div>
   );
 };
