94850bee1fcb5255f19bcd10b93f7729072639f8
Josh Newman <josh.newman4390@gmail.com>
2023-12-04 20:44:15 +1100

    Add react testing library setup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jest-environment-jsdom",
 };
+
+module.exports = createJestConfig(customJestConfig);
